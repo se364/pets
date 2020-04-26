15d861299ca85c376e1f551f588128f6be94a1b7
Shah Emran <semran@mail.greenriver.edu>
2020-04-26 02:03:17 -0400

    controller page is created in pets dirextory as index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Shah Emran
+ * 4/17/2020
+ *
+ * Pair Program 2
+ */
+
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once("vendor/autoload.php");
+
+// Create an instance of the Base Class
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function(){
+    echo '<h1> Pet Home</h1>';
+    }
+);
+
+// Run fat free
+$f3->run();
+
+?>
